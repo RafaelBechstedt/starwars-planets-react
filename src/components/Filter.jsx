26abc8ffea731ffc335e328e9242d138b44428b0
Rafael <bechstedt@hotmail.com>
2022-10-27 02:29:58 -0300

    Requisito 3 e 4 - Lint reclamando no useMemo, não sei como resolver

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,17 +2,66 @@ import React, { useContext } from 'react';
 import MyContext from '../context/MyContext';
 
 function Filter() {
-  const { filteredPlanet, handleInputFilter } = useContext(MyContext);
+  const {
+    filteredPlanet,
+    handleInputFilter,
+    columnFilter,
+    handleColumnFilter,
+    comparisonFilter,
+    handleComparisonFilter,
+    valueFilter,
+    handleValueFilter,
+    handleButtonFilter,
+  } = useContext(MyContext);
+
   return (
-    <label htmlFor="name-filter">
+    <div>
+      <label htmlFor="name-filter">
+        <input
+          type="text"
+          id="name-filter"
+          data-testid="name-filter"
+          value={ filteredPlanet }
+          onChange={ handleInputFilter }
+        />
+      </label>
+
+      <select
+        value={ columnFilter }
+        onChange={ handleColumnFilter }
+        data-testid="column-filter"
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+
+      <select
+        value={ comparisonFilter }
+        onChange={ handleComparisonFilter }
+        data-testid="comparison-filter"
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+
       <input
-        type="text"
-        id="name-filter"
-        data-testid="name-filter"
-        value={ filteredPlanet }
-        onChange={ handleInputFilter }
+        type="number"
+        value={ valueFilter }
+        onChange={ handleValueFilter }
+        data-testid="value-filter"
       />
-    </label>
+      <button
+        type="button"
+        onClick={ handleButtonFilter }
+        data-testid="button-filter"
+      >
+        Filtrar
+      </button>
+    </div>
   );
 }
 
--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -5,6 +5,9 @@ import MyContext from './MyContext';
 function Provider({ children }) {
   const [planetsList, setPlanetsList] = useState([]);
   const [filteredPlanet, setFilteredPlanet] = useState('');
+  const [columnFilter, setColumnFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState(0);
 
   const requestAPI = async () => {
     const response = await fetch('https://swapi.dev/api/planets');
@@ -21,11 +24,48 @@ function Provider({ children }) {
     setFilteredPlanet(value);
   };
 
-  const value = useMemo(
-    () => ({
-      planetsList, filteredPlanet, handleInputFilter }),
-    [planetsList, filteredPlanet],
-  );
+  const handleColumnFilter = ({ target: { value } }) => {
+    setColumnFilter(value);
+  };
+
+  const handleComparisonFilter = ({ target: { value } }) => {
+    setComparisonFilter(value);
+  };
+
+  const handleValueFilter = ({ target: { value } }) => {
+    setValueFilter(value);
+  };
+
+  const handleButtonFilter = () => {
+    if (comparisonFilter === 'maior que') {
+      const filter = planetsList
+        .filter((element) => Number(element[columnFilter]) > Number(valueFilter));
+      setPlanetsList(filter);
+    }
+    if (comparisonFilter === 'menor que') {
+      const filter = planetsList
+        .filter((element) => Number(element[columnFilter]) < Number(valueFilter));
+      setPlanetsList(filter);
+    }
+    if (comparisonFilter === 'igual a') {
+      const filter = planetsList
+        .filter((element) => element[columnFilter] === valueFilter);
+      setPlanetsList(filter);
+    }
+  };
+
+  const value = useMemo(() => ({
+    planetsList,
+    filteredPlanet,
+    columnFilter,
+    comparisonFilter,
+    valueFilter,
+    handleInputFilter,
+    handleColumnFilter,
+    handleComparisonFilter,
+    handleValueFilter,
+    handleButtonFilter,
+  }), [planetsList, filteredPlanet, columnFilter, comparisonFilter, valueFilter]);
 
   return (
     <MyContext.Provider value={ value }>
